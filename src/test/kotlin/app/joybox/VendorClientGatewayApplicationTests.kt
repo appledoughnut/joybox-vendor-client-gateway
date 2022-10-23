5ee33ee40425bb58c87131dd733edecb19ceae37
jonghoon <>
2022-10-23 15:58:19 +0900

    init gateway project

---
+++ src/test/kotlin/app/joybox/VendorClientGatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package app.joybox
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VendorClientGatewayApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
